a9c30bc78a3da6130bfe74c0920db663e3410060
Larry Reaves <larry@yrral.net>
2015-03-13 02:37:15 -0400

    fix form index (for real)

--- lib/annlat/expression_helper.rb
+++ lib/annlat/expression_helper.rb
@@ -257,6 +257,7 @@ module ExpressionHelper
         @form_index = 0
         @form_index += 1 while (@form_index+1)/@forms.size.to_f < diff + 0.2
       end
+      @form_index = Math.min(@form_index, @forms.size - 1)
     end
 
     @form = @forms[@form_index]
