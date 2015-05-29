b819eeecdce4ea6d0c9b68d65c4c950258657f41
Larry Reaves <larry@yrral.net>
2015-05-29 06:46:24 -0400

    use precompiled header to speed up image generation

--- lib/annlat/latex_generate.rb
+++ lib/annlat/latex_generate.rb
@@ -7,11 +7,16 @@ class LatexGenerate < Image
     super("#{@uuid}.png", {dynamic: true})
   end
 
+  def ensure_header
+    unless File.exist?('header.fmt')
+      FileUtils.cp(File.dirname(__FILE__) + , '/header.fmt')
+    end
+  end
+
   def generate_tex_file
     File.open("#{@uuid}.tex", "w") do |f|
-      f.write '\documentclass[convert={density=600,outext=.png}]{standalone}
+      f.write '%&header
 \usepackage{fontspec}
-\setmainfont{lmroman10-regular.otf}
 \begin{document}
 $
 '
@@ -23,8 +28,12 @@ $
     end
   end
 
-  def generate_png_from_tex
+  def generate_pdf_from_tex
     `xelatex -shell-escape #{@uuid}.tex`
+  end
+
+  def generate_png_from_pdf
+    `convert -density 600 #{@uuid}.pdf #{@uuid}.png`
   end
 
   def clean_up_tex_intermediates
@@ -32,8 +41,10 @@ $
   end
 
   def generate_png
+    ensure_header
     generate_tex_file
-    generate_png_from_tex
+    generate_pdf_from_tex
+    generate_png_from_pdf
     clean_up_tex_intermediates
   end
 end
