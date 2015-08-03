8f5601c5947379de18ec168ee6ebde32a3282b06
Larry Reaves <larry@yrral.net>
2015-08-03 09:52:05 -0400

    missed new file, oops

---
+++ lib/annlat/annlat_hotkey.rb
@@ -0,0 +1,35 @@
+# coding: utf-8
+module AnnLatHotkey
+  def self.available_keys
+    [:lparen, :rparen, :exponent, :multiply,
+     :divide, :add, :subtract, :pi, :sqrt]
+  end
+
+  def self.annlat_to_latex_and_input(al)
+    al.options.map do |h|
+      key = h[:sentence_options][:key]
+      [:lparen, :rparen, :exponent, :multiply,
+       :divide, :add, :subtract, :pi, :sqrt]
+      case key
+      when :lparen
+        ['(']*2
+      when :rparen
+        [')']*2
+      when :exponent
+        ['^']*2
+      when :multiply
+        ['×', '\cdot']
+      when :divide
+        [LatexDiv.new('','').latex, '/']
+      when :add
+        ['+']*2
+      when :subtract
+        ['-']*2
+      when :pi
+        [LatexPi.new.latex, '\pi']
+      when :sqrt
+        [LatexSqrt.new('').latex, '\sqrt']
+      end
+    end
+  end
+end
